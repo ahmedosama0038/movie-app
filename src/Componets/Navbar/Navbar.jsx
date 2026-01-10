43a61dafbed6df144f2901b6fef6cc0a281ce93f
Ahmed osama <ah.osama303@gmail.com>
2026-01-10 13:58:49 +0200

    Update validation messages

--- src/Componets/Navbar/Navbar.jsx
+++ src/Componets/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Navbar.css";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
 
 export default function Navbar() {
- const Navigate=  useNavigate()
+  const { token, settoken } = useContext(AuthContext);
+  const Navigate = useNavigate();
 
-function handelLgout(){
-  localStorage.removeItem('token')
-Navigate('login')
-}
+  function handleLogout() {
+    localStorage.removeItem("token");
+    Navigate("/login");
+    settoken(null);
+  }
 
   return (
     <nav className="movie-navbar">
@@ -39,92 +43,99 @@ Navigate('login')
 
         {/* Navigation Links */}
         <div className="nav-links d-md-flex align-items-center">
-          <ul className="nav-list d-md-flex align-items-center mb-0">
-            <li>
-              <NavLink
-                className="nav-link px-2 d-flex align-items-center"
-                to="/login"
-              >
-                <i className="bi bi-box-arrow-in-right me-2" aria-hidden></i>
-                Login
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="nav-link px-2 d-flex align-items-center btn-signup"
-                to="/signup"
-              >
-                <i className="bi bi-person-plus me-2" aria-hidden></i>
-                Signup
-              </NavLink>
-            </li>
+          {!token && (
+            <ul className="nav-list d-md-flex align-items-center mb-0">
+              <li>
+                <NavLink
+                  className="nav-link px-2 d-flex align-items-center"
+                  to="/login"
+                >
+                  <i className="bi bi-box-arrow-in-right me-2" aria-hidden></i>
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="nav-link px-2 d-flex align-items-center btn-signup"
+                  to="/signup"
+                >
+                  <i className="bi bi-person-plus me-2" aria-hidden></i>
+                  Signup
+                </NavLink>
+              </li>
 
-            <li className="dropdown ms-md-2 mt-2 mt-md-0">
-              <NavLink
-                to="/trending"
-                className={({ isActive }) =>
-                  `nav-link dropdown-toggle d-flex align-items-center${
-                    isActive ? " active" : ""
-                  }`
-                }
-                tabIndex="0"
-              >
-                <i className="bi bi-fire me-2"></i>
-                Trending
-                <i className="bi bi-chevron-down ms-2" aria-hidden></i>
-              </NavLink>
-              <ul className="dropdown-menu shadow-sm">
-                <li>
-                  <NavLink
-                    to={'/*'}
-                    className={({ isActive }) =>
-                      `dropdown-item d-flex align-items-center${
-                        isActive ? " active" : ""
-                      }`
-                    }
-                  >
-                    <i className="bi bi-calendar-day me-2"></i>
-                    Today
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/trending/week"
-                    className={({ isActive }) =>
-                      `dropdown-item d-flex align-items-center${
-                        isActive ? " active" : ""
-                      }`
-                    }
-                  >
-                    <i className="bi bi-calendar-week me-2"></i>
-                    This Week
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="/top-rated"
-                    className={({ isActive }) =>
-                      `dropdown-item d-flex align-items-center${
-                        isActive ? " active" : ""
-                      }`
-                    }
-                  >
-                    <i className="bi bi-star me-2"></i>
-                    Top Rated
-                  </NavLink>
-                </li>
-              </ul>
-            </li>
-            <li>
-               <button
-      onClick={handelLgout}
-      className="btn btn-outline-danger d-flex align-items-center gap-2"
-    >
-      <i className="bi bi-box-arrow-right"></i>
-      Logout
-    </button>
-            </li>
-          </ul>
+              <li className="dropdown ms-md-2 mt-2 mt-md-0">
+                <NavLink
+                  to="/trending"
+                  className={({ isActive }) =>
+                    `nav-link dropdown-toggle d-flex align-items-center${
+                      isActive ? " active" : ""
+                    }`
+                  }
+                  tabIndex="0"
+                >
+                  <i className="bi bi-fire me-2"></i>
+                  Trending
+                  <i className="bi bi-chevron-down ms-2" aria-hidden></i>
+                </NavLink>
+                <ul className="dropdown-menu shadow-sm">
+                  <li>
+                    <NavLink
+                      to={"/*"}
+                      className={({ isActive }) =>
+                        `dropdown-item d-flex align-items-center${
+                          isActive ? " active" : ""
+                        }`
+                      }
+                    >
+                      <i className="bi bi-calendar-day me-2"></i>
+                      Today
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/trending/week"
+                      className={({ isActive }) =>
+                        `dropdown-item d-flex align-items-center${
+                          isActive ? " active" : ""
+                        }`
+                      }
+                    >
+                      <i className="bi bi-calendar-week me-2"></i>
+                      This Week
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/top-rated"
+                      className={({ isActive }) =>
+                        `dropdown-item d-flex align-items-center${
+                          isActive ? " active" : ""
+                        }`
+                      }
+                    >
+                      <i className="bi bi-star me-2"></i>
+                      Top Rated
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          )}
+
+          {token && (
+            <ul className=" list-unstyled">
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="btn btn-outline-danger d-flex align-items-center gap-2"
+                >
+                  <i className="bi bi-box-arrow-right"></i>
+                  Logout
+                </button>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </nav>
